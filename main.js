83d07eb94b6da7586ccb95ef47729965a516ebcf
somyaa <somyasharma0488@gmail.com>
2025-07-10 21:24:47 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// Random Fitness Challenge
+
+// Generate a random number between 0 and 3
+let randomNumber = Math.floor(Math.random() * 4);
+
+if (randomNumber === 0) {
+  console.log("10 Push-ups");
+} else if (randomNumber === 1) {
+  console.log("10 Sit-ups");
+} else if (randomNumber === 2) {
+  console.log("10 Squats");
+} else if (randomNumber === 3) {
+  console.log("10 Jumping Jacks");
+} else {
+  console.log("Time to stretch!");
+}
+
+// Don't forget to do your challenge! 🔥💪
